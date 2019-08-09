bcab94c34776b42334f028a3b953e7af3d8b9b83
samuegalindo <48724569+samuegalindo@users.noreply.github.com>
2019-08-09 10:34:52 -0500

    6

--- js/reloj.js
+++ js/reloj.js
@@ -55,7 +55,7 @@
 	actualizarHora();
 	var intervalo = setInterval(actualizarHora, 1000);
 	
-	if (txthora = horas + txtmin == minutos + Formato == ampm) {
+	if (hora = txthora + minutos == txtmin + ampm == Formato) {
 		$('#beep').tap(function(){
 			navigator.notification.beep(1);
 		});//tap beep
